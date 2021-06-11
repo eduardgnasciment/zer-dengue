c88bef74a3d7b7673312662d8209455a976960b3
Eduardo C <82726978+eduardgnasciment@users.noreply.github.com>
2021-06-11 19:42:06 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,27 @@
+<html lang="pt-br">
+<head>
+	 <meta charset="utf-8"/>
+	 <title> ZER@DENGUE LOGIN</title>
+	 <link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+	<div id="corpo-form-cad">
+	<h1>Cadastrar</h1>
+	<form method="POST" action="processa.php">
+		<input type="text" placeholder="Nome Completo">
+		<input type="text" placeholder="RG">
+		<input type="text" placeholder="CPF">
+		<input type="text" placeholder="Telefone">
+		<input type="email" placeholder="Email">
+		<input type="text" placeholder="Endereço">
+		<input type="text" placeholder="Número">
+		<input type="text" placeholder="Bairro">
+		<input type="text" placeholder="Cidade">
+		<input type="text" placeholder="Estado">
+        <input type="password" placeholder="Senha">
+        <input type="password" placeholder="Confirmar Senha" ="">
+        <input type="submit" value="Cadastrar">
+	</form>>
+</div>		
+</body>
+</html>
